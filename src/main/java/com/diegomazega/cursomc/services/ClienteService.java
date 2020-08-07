69d9fa6b366d66eaec349836271ce8e604284b8b
Diego Mazega <diego.mazega@live.com>
2020-08-07 12:56:49 -0300

    Ajustando e testando a deleção de cliente

--- src/main/java/com/diegomazega/cursomc/services/ClienteService.java
+++ src/main/java/com/diegomazega/cursomc/services/ClienteService.java
@@ -60,7 +60,7 @@ public class ClienteService {
 		try {
 		clienteRepository.deleteById(id);
 		}catch(DataIntegrityViolationException event) {
-			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas");
+			throw new DataIntegrityException("Não é possível excluir porque há pedidos relacionados");
 		}
 	}
 	
