9b89cd76f1b2a220291c61c5a2fad58f35a2d73f
Diego Mazega <diego.mazega@live.com>
2020-08-26 13:55:43 -0300

    Validação Costumizada: sem email repetido na atualização do cliente

--- src/main/java/com/diegomazega/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/diegomazega/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.diegomazega.cursomc.domain.Cliente;
+import com.diegomazega.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
