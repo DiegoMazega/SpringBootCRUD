ebc1bd88b17269a6928d51fe29c1cef5ddd37a78
Diego Mazega <diego.mazega@live.com>
2020-09-01 13:44:50 -0300

    Itempedido e Pedido toString

--- src/main/java/com/diegomazega/cursomc/domain/Pedido.java
+++ src/main/java/com/diegomazega/cursomc/domain/Pedido.java
@@ -1,8 +1,11 @@
 package com.diegomazega.cursomc.domain;
 
 import java.io.Serializable;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.Locale;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -135,5 +138,29 @@ public class Pedido implements Serializable {
 			return false;
 		return true;
 	}
+
+	@Override
+	public String toString() {
+		NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt", ""));
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		StringBuilder builder = new StringBuilder();
+		builder.append("Pedido Número: ");
+		builder.append(getId());
+		builder.append(", Data: ");
+		builder.append(sdf.format(getDate()));
+		builder.append(", Cliente: ");
+		builder.append(getCliente().getNome());
+		builder.append(", Situação do Pagamento: ");
+		builder.append(getPagamento().getEstado().getDescricao());
+		builder.append("\nDetalhes:\n");
+		for(ItemPedido ip : getItens()) {
+			builder.append(ip.toString());
+		}
+		builder.append("\nValor Total: ");
+		builder.append(nf.format(getTotal()));
+		return builder.toString();
+	}
+	
+	
 
 }
