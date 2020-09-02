5e5e34d70e995f2d5b1a56ae4d8ecf51cb9f4d95
Diego Mazega <diego.mazega@live.com>
2020-09-02 15:32:55 -0300

    Implementando autorização

--- src/main/java/com/diegomazega/cursomc/config/SecurityConfig.java
+++ src/main/java/com/diegomazega/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.diegomazega.cursomc.security.JWTAuthenticationFilter;
+import com.diegomazega.cursomc.security.JWTAuthorizationFilter;
 import com.diegomazega.cursomc.security.JWTUtil;
 
 @Configuration
@@ -57,6 +58,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.authenticated();
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 	}
 	
 	@Override
