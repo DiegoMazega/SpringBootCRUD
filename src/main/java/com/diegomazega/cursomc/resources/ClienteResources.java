c41321b20158976f4dca8742be05e2ffcb637d41
Diego Mazega <diego.mazega@live.com>
2020-09-10 16:56:59 -0300

    Enviando imagem via endPoint

--- src/main/java/com/diegomazega/cursomc/resources/ClienteResources.java
+++ src/main/java/com/diegomazega/cursomc/resources/ClienteResources.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.diegomazega.cursomc.domain.Cliente;
@@ -79,5 +80,11 @@ public class ClienteResources {
 	public ResponseEntity<Void> delete(@Valid @PathVariable Integer id){
 		clienteService.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@PostMapping(value="/picture")
+	public ResponseEntity<Void> uploadFilePicture(@RequestParam(name="file") MultipartFile file){
+		URI uri = clienteService.uploadProfilePicture(file);
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/diegomazega/cursomc/services/S3Service.java
+++ src/main/java/com/diegomazega/cursomc/services/S3Service.java
@@ -1,40 +1,53 @@
 package com.diegomazega.cursomc.services;
 
-import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
-import com.amazonaws.AmazonClientException;
-import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 
 @Service
 public class S3Service {
-	
+
 	@Autowired
 	private AmazonS3 s3client;
-	
+
 	@Value("${s3.bucket}")
 	private String bucketName;
-	
+
 	private Logger LOG = LoggerFactory.getLogger(S3Service.class);
-	
-	public void uploadFile(String localFilePath) {
+
+	public URI uploadFile(MultipartFile multiPartFile) {
 		try {
-			File file = new File(localFilePath);
+			String fileName = multiPartFile.getOriginalFilename();
+			InputStream inputStream;
+			inputStream = multiPartFile.getInputStream();
+			String contentType = multiPartFile.getContentType();
+			return uploadFile(inputStream, fileName, contentType);
+		} catch (IOException e) {
+			throw new RuntimeException("Erro de IO: " + e.getMessage());
+		}
+	}
+
+	public URI uploadFile(InputStream inputStream, String fileName, String contentType) {
+		try {
+			ObjectMetadata omd = new ObjectMetadata();
+			omd.setContentType(contentType);
 			LOG.info("Iniciando o Upload");
-			s3client.putObject(new PutObjectRequest(bucketName, "teste.jpg", file));
+			s3client.putObject(bucketName, fileName, inputStream, omd);
 			LOG.info("Fim do Upload");
-		}catch(AmazonServiceException event) {
-			LOG.info("AmazonServiceException: " + event.getErrorMessage());
-			LOG.info("Status Code: " + event.getErrorCode());
-		}catch(AmazonClientException event) {
-			LOG.info("AmazonServiceException: " + event.getMessage());
+			return s3client.getUrl(bucketName, fileName).toURI();
+		} catch (URISyntaxException e) {
+			throw new RuntimeException("Erro ao converter URL para URI");
 		}
 	}
 }
