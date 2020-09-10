b29ea06830d63182c2224b84fbdc1413275199bf
Diego Mazega <diego.mazega@live.com>
2020-09-09 23:02:56 -0300

    Salvando arquivo no S3

--- src/main/java/com/diegomazega/cursomc/CursoMcApplication.java
+++ src/main/java/com/diegomazega/cursomc/CursoMcApplication.java
@@ -1,12 +1,17 @@
 package com.diegomazega.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.diegomazega.cursomc.services.S3Service;
+
 @SpringBootApplication
 public class CursoMcApplication implements CommandLineRunner {
-
+	
+	@Autowired
+	private S3Service s3service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoMcApplication.class, args);
@@ -14,8 +19,7 @@ public class CursoMcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		
-		
+		s3service.uploadFile("C:\\Users\\diego\\Pictures\\Golfinhos.jpg");
 	}
 
 }
